1807763d7eabcaa4f7156ef25fd70ea57daa0b8e
Mateus-msj3 <mateussouzaads@outlook.com>
2022-11-02 20:40:46 -0300

    Estrutura e configurações iniciais

---
+++ src/main/java/com/io/github/msj/mscurso/model/Curso.java
@@ -0,0 +1,34 @@
+package com.io.github.msj.mscurso.model;
+
+import com.io.github.msj.mscurso.enums.SituacaoCurso;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "É necessário informar o nome.")
+    private String nome;
+
+    @NotEmpty(message = "É necessário informar o número de vagas.")
+    private Integer numeroVagas;
+
+    @Enumerated(value = EnumType.STRING)
+    private SituacaoCurso situacaoCurso;
+
+    @Embedded
+    private Inscricao inscricao;
+}
